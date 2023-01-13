c78897fa29a54af0b3742e56c27c6ffe1cd677be
mzorn-58 <66443989+mzorn-58@users.noreply.github.com>
2023-01-13 18:18:11 -0500

    small code update

--- tests/testthat/test-create_exp_target.R
+++ tests/testthat/test-create_exp_target.R
@@ -4,12 +4,11 @@ print(getwd())
 library (usethis)
 
 test_that("correct format does not produce error", {
-  
+
   expect_no_error(suppressMessages(create_exp_target(models = "UMass-trends_ensemble",
                                                   source = "local_hub_repo", 
                                                   hub_repo="~/GitHub/flu-hosp-models-2021-2022",
                                                   data_processed="./weekly-submission/forecasts/",
-                                                  c_target="2 wk flu hosp rate change",
                                                   output1="~/GitHub/Flusight-forecast-data/data-experimental/tests/",
                                                   output2="~/GitHub/flu-hosp-models-2021-2022/weekly-submission/forecasts/data-experimental/tests/",
                                                   output3="~/GitHub/flu-hosp-models-2021-2022/weekly-submission/forecasts/data-experimental/plots/tests/")))
@@ -21,7 +20,6 @@ test_that("incorrect model name should throw error", {
                                                   source = "local_hub_repo", 
                                                   hub_repo="~/GitHub/flu-hosp-models-2021-2022",
                                                   data_processed="./weekly-submission/forecasts/",
-                                                  c_target="2 wk flu hosp rate change",
                                                   output1="~/GitHub/Flusight-forecast-data/data-experimental/tests/",
                                                   output2="~/GitHub/flu-hosp-models-2021-2022/weekly-submission/forecasts/data-experimental/tests/",
                                                   output3="~/GitHub/flu-hosp-models-2021-2022/weekly-submission/forecasts/data-experimental/plots/tests/")))
@@ -32,7 +30,6 @@ test_that("missing hub_repo should throw error", {
   expect_error(suppressMessages(create_exp_target(models = "UMass-trends_ensemble",
                                                   source = "local_hub_repo",
                                                   data_processed="./weekly-submission/forecasts/",
-                                                  c_target="2 wk flu hosp rate change",
                                                   output1="~/GitHub/Flusight-forecast-data/data-experimental/tests/",
                                                   output2="~/GitHub/flu-hosp-models-2021-2022/weekly-submission/forecasts/data-experimental/tests/",
                                                   output3="~/GitHub/flu-hosp-models-2021-2022/weekly-submission/forecasts/data-experimental/plots/tests/")))
@@ -44,7 +41,6 @@ test_that("incorrect hub_repo should throw error", {
                                                   source = "local_hub_repo",
                                                   hub_repo="xxx",
                                                   data_processed="./weekly-submission/forecasts/",
-                                                  c_target="2 wk flu hosp rate change",
                                                   output1="~/GitHub/Flusight-forecast-data/data-experimental/tests/",
                                                   output2="~/GitHub/flu-hosp-models-2021-2022/weekly-submission/forecasts/data-experimental/tests/",
                                                   output3="~/GitHub/flu-hosp-models-2021-2022/weekly-submission/forecasts/data-experimental/plots/tests/")))
@@ -56,7 +52,6 @@ test_that("missing data_processed should throw error", {
   expect_error(suppressMessages(create_exp_target(models = "UMass-trends_ensemble",
                                                   source = "local_hub_repo", 
                                                   hub_repo="~/GitHub/flu-hosp-models-2021-2022",
-                                                  c_target="2 wk flu hosp rate change",
                                                   output1="~/GitHub/Flusight-forecast-data/data-experimental/tests/",
                                                   output2="~/GitHub/flu-hosp-models-2021-2022/weekly-submission/forecasts/data-experimental/tests/",
                                                   output3="~/GitHub/flu-hosp-models-2021-2022/weekly-submission/forecasts/data-experimental/plots/tests/")))
@@ -68,22 +63,11 @@ test_that("incorrect data_processed should throw error", {
                                                   source = "local_hub_repo", 
                                                   hub_repo="~/GitHub/flu-hosp-models-2021-2022",
                                                   data_processed="xxx",
-                                                  c_target="2 wk flu hosp rate change",
                                                   output1="~/GitHub/Flusight-forecast-data/data-experimental/tests/",
                                                   output2="~/GitHub/flu-hosp-models-2021-2022/weekly-submission/forecasts/data-experimental/tests/",
                                                   output3="~/GitHub/flu-hosp-models-2021-2022/weekly-submission/forecasts/data-experimental/plots/tests/")))
 })
 
-test_that("missing c_target should throw error", {
-  
-  expect_error(suppressMessages(create_exp_target(models = "UMass-trends_ensemble",
-                                                  source = "local_hub_repo", 
-                                                  data_processed="./weekly-submission/forecasts/",
-                                                  hub_repo="~/GitHub/flu-hosp-models-2021-2022",
-                                                  output1="~/GitHub/Flusight-forecast-data/data-experimental/tests/",
-                                                  output2="~/GitHub/flu-hosp-models-2021-2022/weekly-submission/forecasts/data-experimental/tests/",
-                                                  output3="~/GitHub/flu-hosp-models-2021-2022/weekly-submission/forecasts/data-experimental/plots/tests/")))
-})
 
 test_that("missing output1 should throw error", {
   
@@ -91,19 +75,17 @@ test_that("missing output1 should throw error", {
                                                   source = "local_hub_repo", 
                                                   data_processed="./weekly-submission/forecasts/",
                                                   hub_repo="~/GitHub/flu-hosp-models-2021-2022",
-                                                  c_target="2 wk flu hosp rate change",
                                                   output2="~/GitHub/flu-hosp-models-2021-2022/weekly-submission/forecasts/data-experimental/tests/",
                                                   output3="~/GitHub/flu-hosp-models-2021-2022/weekly-submission/forecasts/data-experimental/plots/tests/")))
 })
 
-test_that("incorrect output1 should throw error", {
+test_that("incorrect output1 path should throw warning", {
   
   expect_error(suppressMessages(create_exp_target(models = "UMass-trends_ensemble",
                                                   source = "local_hub_repo", 
                                                   data_processed="./weekly-submission/forecasts/",
                                                   hub_repo="~/GitHub/flu-hosp-models-2021-2022",
-                                                  c_target="2 wk flu hosp rate change",
-                                                  output1="xxx",
+                                                  output1="~/GitHub/flu-hosp-models-2021-2022/weekly-submission/xxx/",
                                                   output2="~/GitHub/flu-hosp-models-2021-2022/weekly-submission/forecasts/data-experimental/tests/",
                                                   output3="~/GitHub/flu-hosp-models-2021-2022/weekly-submission/forecasts/data-experimental/plots/tests/")))
 })
@@ -114,19 +96,18 @@ test_that("missing output2 should NOT throw error", {
                                                   source = "local_hub_repo", 
                                                   data_processed="./weekly-submission/forecasts/",
                                                   hub_repo="~/GitHub/flu-hosp-models-2021-2022",
-                                                  c_target="2 wk flu hosp rate change",
+                                                  output1="~/GitHub/Flusight-forecast-data/data-experimental/tests/",
                                                   output3="~/GitHub/flu-hosp-models-2021-2022/weekly-submission/forecasts/data-experimental/plots/tests/")))
 })
 
-test_that("incorrect output2 should throw error", {
+test_that("incorrect output2 path should throw error", {
   
   expect_error(suppressMessages(create_exp_target(models = "UMass-trends_ensemble",
                                                   source = "local_hub_repo", 
                                                   data_processed="./weekly-submission/forecasts/",
                                                   hub_repo="~/GitHub/flu-hosp-models-2021-2022",
-                                                  c_target="2 wk flu hosp rate change",
                                                   output1="~/GitHub/Flusight-forecast-data/data-experimental/tests/",
-                                                  output2="xxx",
+                                                  output2="~/GitHub/flu-hosp-models-2021-2022/weekly-submission/xxx/",
                                                   output3="~/GitHub/flu-hosp-models-2021-2022/weekly-submission/forecasts/data-experimental/plots/tests/")))
 })
 
@@ -136,19 +117,65 @@ test_that("missing output3 should throw error", {
                                                   source = "local_hub_repo", 
                                                   data_processed="./weekly-submission/forecasts/",
                                                   hub_repo="~/GitHub/flu-hosp-models-2021-2022",
-                                                  c_target="2 wk flu hosp rate change",
                                                   output1="~/GitHub/Flusight-forecast-data/data-experimental/tests/",
                                                   output2="~/GitHub/flu-hosp-models-2021-2022/weekly-submission/forecasts/data-experimental/tests/")))
 })
 
-test_that("incorrect output3 should throw error", {
+test_that("incorrect output3 path should throw error", {
   
   expect_error(suppressMessages(create_exp_target(models = "UMass-trends_ensemble",
                                                   source = "local_hub_repo", 
                                                   data_processed="./weekly-submission/forecasts/",
                                                   hub_repo="~/GitHub/flu-hosp-models-2021-2022",
-                                                  c_target="2 wk flu hosp rate change",
                                                   output1="~/GitHub/Flusight-forecast-data/data-experimental/tests/",
                                                   output2="~/GitHub/flu-hosp-models-2021-2022/weekly-submission/forecasts/data-experimental/tests/",
-                                                  output3="xxx")))
+                                                  output3="~/GitHub/flu-hosp-models-2021-2022/weekly-submission/xxx/")))
+})
+
+test_that("value for experimental target add to 1 by location", {
+  
+ exp_t<-create_exp_target(models = "UMass-trends_ensemble",
+                                                     source = "local_hub_repo", 
+                                                     hub_repo="~/GitHub/flu-hosp-models-2021-2022",
+                                                     data_processed="./weekly-submission/forecasts/",
+                                                     output1="~/GitHub/Flusight-forecast-data/data-experimental/tests/",
+                                                     output2="~/GitHub/flu-hosp-models-2021-2022/weekly-submission/forecasts/data-experimental/tests/",
+                                                     output3="~/GitHub/flu-hosp-models-2021-2022/weekly-submission/forecasts/data-experimental/plots/tests/")
+  d<- exp_t  %>%
+    group_by(location_name) %>%
+    summarize(y=sum(value)) 
+  observe<-d[,2]
+  o_num<-as.numeric(unlist(observe))
+  sum_o<-sum(o_num) 
+  
+  n<-nrow(d)
+  expect<-rep(c(1),each=n)
+  sum_e<-sum(expect)
+  
+  expect_equal(sum_o,sum_e)
+
+})
+
+test_that("value for experimental target add to 1 by location", {
+  
+  exp_t<-create_exp_target(models = "UMass-trends_ensemble",
+                           source = "local_hub_repo", 
+                           hub_repo="~/GitHub/flu-hosp-models-2021-2022",
+                           data_processed="./weekly-submission/forecasts/",
+                           output1="~/GitHub/Flusight-forecast-data/data-experimental/tests/",
+                           output2="~/GitHub/flu-hosp-models-2021-2022/weekly-submission/forecasts/data-experimental/tests/",
+                           output3="~/GitHub/flu-hosp-models-2021-2022/weekly-submission/forecasts/data-experimental/plots/tests/")
+  d<- exp_t  %>%
+    group_by(location_name) %>%
+    summarize(y=sum(value)) 
+  observe<-d[,2]
+  o_num<-as.numeric(unlist(observe))
+  sum_o<-sum(o_num) 
+  
+  n<-nrow(d)
+  expect<-rep(c(1),each=n)
+  sum_e<-sum(expect)
+  
+  expect_equal(sum_o,sum_e)
+  
 })
